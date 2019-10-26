d81378596a9cc7686c187cca350050c13ff72095
petrovale <leha.isakov@yandex.ru>
2019-10-26 17:28:41 +0300

    implement New Inventory Listener

---
+++ src/main/java/guru/springboot/mssc/beer/inventory/service/services/NewInventoryListener.java
@@ -0,0 +1,31 @@
+package guru.springboot.mssc.beer.inventory.service.services;
+
+import guru.springboot.mssc.beer.inventory.service.config.JmsConfig;
+import guru.springboot.mssc.beer.inventory.service.domain.BeerInventory;
+import guru.springboot.mssc.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.springboot.mssc.common.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NewInventoryListener {
+
+  private final BeerInventoryRepository beerInventoryRepository;
+
+  @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+  public void listen(NewInventoryEvent event){
+
+    log.debug("Got Inventory: " + event.toString());
+
+    beerInventoryRepository.save(BeerInventory.builder()
+        .beerId(event.getBeerDto().getId())
+        .upc(event.getBeerDto().getUpc())
+        .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+        .build());
+  }
+
+}
---
+++ src/main/java/guru/springboot/mssc/common/events/BeerEvent.java
@@ -0,0 +1,18 @@
+package guru.springboot.mssc.common.events;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class BeerEvent implements Serializable {
+
+  static final long serialVersionUID = -5781515597148163111L;
+
+  private BeerDto beerDto;
+}
